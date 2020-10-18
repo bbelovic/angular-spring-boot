6ba2adbce9e0aef77d5b6ff2f52d32080f4e840e
bbelovic <bbelovic@gmail.com>
2020-10-18 23:13:31 +0200

    Trobleshooting logout issue

--- notes-fe/src/proxy.conf.js
+++ notes-fe/src/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
       context: ['/user', '/api', '/oauth2', '/login'],
-      target: 'http://localhost:8080',
+      target: 'http://localhost:4200',
       secure: false,
       logLevel: 'debug'
     }
