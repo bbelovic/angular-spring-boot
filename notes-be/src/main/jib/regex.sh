9b125c8ce4e9653c94f2a5fb3f2560a93a6d84e1
bbelovic <bbelovic@gmail.com>
2020-11-11 21:11:38 +0100

    Working regex

--- notes-be/src/main/jib/regex.sh
+++ notes-be/src/main/jib/regex.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-jdbc:postgresql://db:5432/notes_db
 
-.*://[.*:\d{4,5}]/.*
-
-echo 'jdbc:postgresql://db:5432/notes_db' | grep -o '.*://[.*]:5432/.*'
-echo 'jdbc:db:5432/notes_db' | grep -o '.*:[.*]:5432/.*'
+input="postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90"
 
 
-postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90
+strname="jdbc:postgresql://127.0.0.1:5432/notes_db"
+if [[ $input =~ .*//(.+):5432.* ]]; then
+    echo "result: ${BASH_REMATCH[1]}"
+else
+    echo "unable to parse string $strname"
+fi
+
+
+#postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90
