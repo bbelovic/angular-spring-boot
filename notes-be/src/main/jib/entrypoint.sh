82047b1272c1b7c7cdc0521d5627e22955322bdf
bbelovic <bbelovic@gmail.com>
2020-11-12 21:35:25 +0100

    Parsing db hostname before passing it to wait script

--- notes-be/src/main/jib/entrypoint.sh
+++ notes-be/src/main/jib/entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-P=$1
-RES=$(./wait-for-it.sh $P --timeout=5 -q)
+
+hostname="n/a"
+
+parse_hostname() {
+  echo "Processing input url: $1"
+  if [[ $1 =~ .*//(.+:[0-9]{4}).* ]]; then
+    hostname=${BASH_REMATCH[1]}
+else
+    hostname="Unable to parse url: $1"
+fi
+}
+
+parse_hostname $SPRING_DATASOURCE_URL
+
+RES=$(./wait-for-it.sh $hostname --timeout=5 -q)
 RES=$?
 
 if [[ $RES -eq 0 ]]; then
 	java -cp /app/resources:/app/classes:/app/libs/* com.okta.developer.notes.backend.NotesBeApplicationKt
 else
-	echo "Unable to ping $P"
+	echo "Unable to ping: $hostname"
 fi	
