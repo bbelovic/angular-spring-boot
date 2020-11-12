3c3a9ea666ed59ea9c3d1e62ac6b7acc185e41af
bbelovic <bbelovic@gmail.com>
2020-11-12 21:36:06 +0100

    Updating regex notes

--- notes-be/src/main/jib/regex.sh
+++ notes-be/src/main/jib/regex.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-input="postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90"
-
-
-strname="jdbc:postgresql://127.0.0.1:5432/notes_db"
-if [[ $input =~ .*//(.+):5432.* ]]; then
-    echo "result: ${BASH_REMATCH[1]}"
+hostname="n/a"
+parse_hostname() {
+  echo "Processing input url: $1"
+  if [[ $1 =~ .*//(.+:[0-9]{4}).* ]]; then
+    hostname=${BASH_REMATCH[1]}
 else
-    echo "unable to parse string $strname"
+    hostname="unable to parse string $1"
 fi
+}
 
+input="postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90"
+parse_hostname $input
+echo "hostname: $hostname"
 
-#postgres://rvwxqhvtvwgqmv:0c43a8597dd04d7efe966ffc64a0892d787e07d4151242109c2c204afb701772@ec2-107-21-109-15.compute-1.amazonaws.com:5432/d4fq7bp9lrok90
+#strname="jdbc:postgresql://127.0.0.1:5432/notes_db"
+#if [[ $input =~ .*//(.+):5432.* ]]; then
+#    echo "result: ${BASH_REMATCH[1]}"
+#else
+#    echo "unable to parse string $strname"
+#fi
+
